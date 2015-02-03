ec0631ea0f169177d1c3cd8f126df7b0d53e2ded
Ryan Swedal <ryanswedal@gmail.com>
2015-02-02 19:16:35 -0600

    add completed static mockups

--- mockups/js/main.js
+++ mockups/js/main.js
@@ -1,5 +1,22 @@
-$(function() {
+( function( window, $, undefined ) {
+'use strict';
+
+    // Mobile Menu Toggle
+    $('.global-nav .toggle').click(function(e){
+        $('.global-nav').toggleClass('active');
+        e.preventDefault();
+    });
+
+    // Sidebar Accordions
+    $('.accordion-toggle').click(function() {
+        $('.accordion-toggle').removeClass('open');
+        $('.accordion-content').slideUp('fast');
+        if($(this).next().is(':hidden') === true) {
+            $(this).addClass('open');
+            $(this).next().slideDown('fast');
+        }
+        return false;
+    });
 
 
-
-});
+})( this, jQuery );
