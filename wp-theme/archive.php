2c0984b20b5952ce13a55e05b174cd0aa9961410
Charlie Rosenbury <gmail@rosenbury.com>
2015-02-04 17:53:18 -0600

    add archive title, fix list width

--- wp-theme/archive.php
+++ wp-theme/archive.php
@@ -16,27 +16,41 @@ get_header();
 
             </div><!-- .banner-photo -->
 
-            <?php
-
-            while(have_posts()){
-                the_post();
-                ?>
-
-                <div class="post">
-
-                    <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
-
-                    <p class="meta">Posted <?php echo get_the_date() ?></p>
-
-                    <?php the_excerpt() ?>
-
-                    <p><a class="button" href="<?php the_permalink() ?>">Read More&hellip;</a></p>
-
-                </div>
+            <div class="primary-content">
 
                 <?php
-            }
-            ?>
+
+                //determine page title
+                
+                if(is_category())
+                    $title = single_cat_title('', false);
+                elseif(is_month())
+                    $title = get_the_date('F Y');
+
+                echo "<h1>$title</h1>";
+
+                while(have_posts()){
+                    the_post();
+                    ?>
+
+                    <div class="post">
+
+                        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+
+                        <p class="meta">Posted <?php echo get_the_date() ?></p>
+
+                        <?php the_post_thumbnail() ?>
+
+                        <?php the_excerpt() ?>
+
+                        <p><a class="button" href="<?php the_permalink() ?>">Read More&hellip;</a></p>
+
+                    </div>
+
+                    <?php
+                }
+                ?>
+            </div>
 
             <div class="secondary-content">
 
